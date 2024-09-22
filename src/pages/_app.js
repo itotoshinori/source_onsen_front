ed4dfcf1ef617d19b3968124ae5e186c35067ccb
Toshinori-Ito <tito40358@gmail.com>
2024-09-22 21:37:20 +0900

    SPAの連携再設定

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,19 @@
 import 'tailwindcss/tailwind.css'
 import { fetchCsrfToken } from '../lib/laravelAxios'
-import { useEffect } from 'react';
+import { useEffect } from 'react'
 
 const App = ({ Component, pageProps }) => {
     const init = async () => {
-        await fetchCsrfToken(); // CSRFトークンを取得
+        await fetchCsrfToken() // CSRFトークンを取得
         // 他の処理を続ける
     }
 
     // 初回レンダリング時にCSRFトークンを取得
     useEffect(() => {
-        init();
-    }, []);
+        init()
+    }, [])
 
     return <Component {...pageProps} />
 }
 
-export default App;
+export default App
