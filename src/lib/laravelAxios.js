3218cbec6b3112cf0325b89294ad75e5f7edd107
Toshinori-Ito <tito40358@gmail.com>
2024-09-22 13:21:27 +0900

    index.js修正

--- src/lib/laravelAxios.js
+++ src/lib/laravelAxios.js
@@ -1,17 +1,18 @@
 import axios from 'axios'
+
 let csrfToken = '' // CSRFトークンの一時的な保存場所
 
 const laravelAxios = axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
+        'X-CSRF-TOKEN': csrfToken, // 正しい位置に移動
     },
     withCredentials: true,
-    'X-CSRF-TOKEN': csrfToken,
-    //withXSRFToken: true,
 })
 
 export const setCsrfToken = token => {
+    // 括弧を削除
     csrfToken = token
 }
 
